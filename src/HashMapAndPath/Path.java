0e1165f7d975d853bc9aef7b6875fe3f80ac5a01
Jonathan Esquivel Sánchez <jesquivelsa@gmail.com>
2018-09-24 21:39:25 -0600

    Code Share @isolis2000

--- src/HashMapAndPath/Path.java
+++ src/HashMapAndPath/Path.java
@@ -7,11 +7,26 @@ public class Path {
 	public void figurerecognition() {
 		
 		NodeLinesPath lastEntry = Main.path.firstNode;
-		String point = lastEntry.matrix.firstNode.point1;
+		String point1 = lastEntry.matrix.firstNode.point1;
+		String point2 = lastEntry.matrix.firstNode.point1;
 		
-		while(point != "") {
+	    PathList pathLocal = new PathList();
+	    
+	    while (lastEntry != null) {
+	    	
+	    	if (lastEntry.matrix.firstNode.point1 == point1 & lastEntry.matrix.firstNode.point2 == point2) {
+	    		
+	    	}else {
+	    		
+	    		pathLocal.addPathList(new ListHashMap(lastEntry.matrix.firstNode.point1,lastEntry.matrix.firstNode.point2));
+	    	}
+	    }
+		
+	    NodeLinesPath localCurrent = pathLocal.firstNode;
+	    
+		while(point1 != "") {
 			
-			point = isInNode(point,lastEntry);
+			point1 = isInNode(localCurrent.matrix.firstNode.point1,localCurrent);
 			
 			
 		}
