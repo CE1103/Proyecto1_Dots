aae7f492989fce5f6e5a8c8b37ed3b4955aa1ad1
Jonathan Esquivel Sánchez <jesquivelsa@gmail.com>
2018-10-03 02:14:36 -0600

    .

---
+++ src/HashMapAndPath/ListPathCreated.java
@@ -0,0 +1,19 @@
+package HashMapAndPath;
+
+public class ListPathCreated {
+
+	public NodePathCreated firstNode;
+	
+	public void addListPathCreated(DotsToLinesList matrix) {
+
+		if (firstNode!=null) {
+			NodePathCreated newPCNode = new NodePathCreated(matrix);
+			newPCNode.next = firstNode;
+			firstNode = newPCNode;
+		}else {
+			firstNode = new NodePathCreated(matrix);
+		}
+	}
+	
+	
+}
---
+++ src/HashMapAndPath/NodePathCreated.java
@@ -0,0 +1,15 @@
+package HashMapAndPath;
+
+public class NodePathCreated {
+
+	DotsToLinesList matrix;
+	NodePathCreated next;
+	
+	public NodePathCreated(DotsToLinesList matrix) {
+		
+		this.matrix = matrix;
+		
+	}
+	
+	
+}
